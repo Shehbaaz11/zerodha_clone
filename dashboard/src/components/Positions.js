ea3346ef0cd7e9c2f826acf9e6f207854774a05e
Shehbaaz11 <162619054+Shehbaaz11@users.noreply.github.com>
2025-07-30 20:33:37 +0530

    Update Positions.js

--- dashboard/src/components/Positions.js
+++ dashboard/src/components/Positions.js
@@ -10,7 +10,7 @@ const Positions = () => {
 const [allPosition , setAllPosition] = useState([])
 
 useEffect(()=>{
-  axios.get("http://localhost:3002/allPositions").then((res)=>{
+  axios.get("https://zerodha-backend-tija.onrender.com/allPositions").then((res)=>{
    console.log(res.data)
     setAllPosition(res.data);
   })
