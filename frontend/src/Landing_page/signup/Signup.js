0574b0a643779c3c5121534ca7b7cc294ee19e20
Shehbaaz11 <162619054+Shehbaaz11@users.noreply.github.com>
2025-07-30 18:57:19 +0530

    Update Signup.js

--- frontend/src/Landing_page/signup/Signup.js
+++ frontend/src/Landing_page/signup/Signup.js
@@ -35,7 +35,7 @@ const Signup = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:3002/user/signup",
+        "https://zerodha-backend-tija.onrender.com/user/signup",
         inputValue,
         { withCredentials: true }
       );
